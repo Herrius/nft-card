ffe75954a49b5b8b06f411f4b6a20303d8a1f835
Herrius <76927894@continental.edu.pe>
2022-03-01 13:07:43 -0500

    diseño movil nft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import './App.js';
+import './styles/styles.css'
+import card from './images/image-equilibrium.jpg'
+import iconETH from './images/icon-ethereum.svg'
+import iconClock from './images/icon-clock.svg'
+import avatar from './images/image-avatar.png'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='card'>
+      <div className='card-img'>
+        <img src={card} alt='image'></img>
+      </div>
+      <div className='description'>
+        <p className='title'>Equilibrium #3429</p>
+        <p className='text'>Our Equilibrium collection promotes balance and calm.</p>
+        <div className='offer'>
+          <span className='price'><img src={iconETH} alt='icon ETH'></img>0.041 ETH</span>
+          <span className='time'><img src={iconClock} alt="Countdown"></img>3 days left</span>
+        </div>
+      </div>
+
+      <footer className='card-footer'>
+        <img src={avatar} alt='profile' className='profile'></img>
+        Creation of <span className='marker'>Jules Wyvern</span>
+      </footer>
+    </section>
   );
 }
 
