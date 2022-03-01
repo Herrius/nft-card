a87077ad56b5b6aa1b026476bbef21f56daf7644
Herrius <76927894@continental.edu.pe>
2022-03-01 16:23:53 -0500

    fin del ejercicio en react

--- src/App.js
+++ src/App.js
@@ -5,14 +5,19 @@ import card from './images/image-equilibrium.jpg'
 import iconETH from './images/icon-ethereum.svg'
 import iconClock from './images/icon-clock.svg'
 import avatar from './images/image-avatar.png'
+import view from './images/icon-view.svg'
 function App() {
   return (
     <section className='card'>
       <div className='card-img'>
         <img src={card} alt='image'></img>
+        <div className='overlay'>
+          <img src={view} alt='icon view'></img>
+          <div></div>
+        </div>
       </div>
       <div className='description'>
-        <p className='title'>Equilibrium #3429</p>
+        <p className='title'>Equilibrium <span>#3429</span></p>
         <p className='text'>Our Equilibrium collection promotes balance and calm.</p>
         <div className='offer'>
           <span className='price'><img src={iconETH} alt='icon ETH'></img>0.041 ETH</span>
